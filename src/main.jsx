f0ea6d1111ff75f896331bc0cd780323542d5f73
sasivarman-s <sureshraman2680@gmail.com>
2026-08-23 20:14:40 +0530

    some components deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react"
 import ReactDom from "react-dom/client"
-import Header from "./components/Header"
-import Body from "./components/Body"
+
 
 
 
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/><Body/></>)
+reactroot.render(<></>)
 
 
