bcccb637956cb86d8120b8501c9e91dafb31db6c
sasivarman-s <sureshraman2680@gmail.com>
2026-08-23 21:15:29 +0530

    props from list implemented by map() method

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDom from "react-dom/client"
 import "./main.css"
 
-function Body(){
+function Body(props){
     return(
         <>
-        <section className="max-w-[100%] h-[600px]">
-            
-        </section>
+        <div className="max-w-[100%] grid grid-cols-2">
+            <div className="w-[200px] h-[200px] m-5 bg-green-500 border rounded-lg">
+                <h1 className="text-center text-2xl">{props.title}</h1>
+                <h2 className="text-center text-2xl">{props.des}</h2>
+            </div>
+        </div>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,37 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Body from "./components/Body"
 
-
+var datamart = [
+    {
+        title : "HTML",
+        des : "hyper text markup language"
+    },
+    {
+        title : "CSS",
+        des : "casecading style sheet"
+    },
+    {
+        title : "JS",
+        des : "Javascript"
+    },
+    {
+        title : "React.js",
+        des : "javascript UI library"
+    }
+]
 
 
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/><Body/><Footer/></>)
+reactroot.render(<><Header/>
+
+{
+    datamart.map((item,index)=>{
+        return(
+            <Body title={item.title} des={item.des} key={item.index}/>
+        )
+    })
+}
+
+<Footer/></>)
 
 
