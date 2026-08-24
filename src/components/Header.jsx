21917cecce437d977dcdc82f92c7d86fed0c5f53
sasivarman-s <sureshraman2680@gmail.com>
2026-08-24 20:27:44 +0530

    live card creation through usestate(),list,onchange(),onclick()

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,57 @@
 import React, { useState } from "react"
 import ReactDom from "react-dom/client"
 import "./main.css"
+import { stringify } from "postcss"
 
 
 
-function Header(){
-    var [valid,setValid] = useState(true)
-    return(
+function Header() {
+    var [userdata, setUserdata] = useState([])
+
+    var [user, setUser] = useState({
+        name : "",
+        age : ""
+    })
+    return (
         <>
-        <div className="bg-slate-300 p-10 mx-auto my-auto max-w-[70%]:" >
-            <h1 className="text-center text-5xl">{valid ? "true" : "flase"}</h1>
-            <button onClick={()=>{
-                setValid(!valid)
-            }} className={` ${valid ?"bg-green-600" : "bg-red-500" } text-center text-2xl  text-white p-2 rounded-lg`}>change</button>
-        </div>
+            <div className="p-10 bg-lime-200 space-y-5 rounded-lg max-w-[70%] mx-auto">
+                <div>
+                    <label> Name:</label>
+                    <input placeholder="enter your name?" className="p-2 outline-none rounded-md" onChange={(e)=>{
+                        
+                        setUser((prev)=>{
+                            return {...prev, name : e.target.value}
+                        })
+                    }}></input>
+                </div>
+                <div>
+                    <label> age:</label>
+                    <input placeholder="enter your age?" className="p-2 outline-none rounded-md" onChange={(e)=>{
+                        setUser((prev)=>{
+                            return {...prev,age: e.target.value}
+                        })
+                    }} ></input>
+                </div>
+                <div>
+                    <button className="bg-purple-600 text-2xl text-white p-2 rounded" onClick={()=>{
+                        setUserdata((prev)=>{
+                            return [...prev,user]
+                        })
+                    }} >post!</button>
+                </div>
+                {
+                    userdata.map((item,i)=>{
+                        return (
+                            <>
+                            <div className="p-2 m-1 bg-cyan-300 rounded-lg w-[200px] h-[200px]">
+                                <h1 className="text-white text-center text-3xl">{item.name}</h1>
+                                <h2 className="text-white text-center text-2xl">{item.age}</h2>
+                            </div>
+                            </>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
