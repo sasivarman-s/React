bb527579f6e8d04e91f0d59541daeb81b434e1eb
sasivarman-s <sureshraman2680@gmail.com>
2026-08-24 12:46:50 +0530

    usestate() performed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react"
 import ReactDom from "react-dom/client"
 import "./main.css"
 
+
+
 function Header(){
+    var [name, setName] = useState("sasi")
+    var [age , setAge] = useState(17)
     return (
         <>
-        <div className="max-w-[100%] bg-yellow-100">
-            <div className="w-[80%]  mx-auto">
-                <h1 className="text-4xl text-blue-600 text-center p-1" >This is SV react.js practice page!</h1>
-            </div>
-        </div>
+        <h1 className="text-center text-4xl text-blue-700">{name}</h1>
+        <h1 className="text-center text-4xl text-blue-700">{age}</h1>
+        <button onClick={()=>{
+            setName("sasivarman")
+            setAge(18)
+        }} >
+            change name
+        </button>
         </>
     )
 }
+
 
 export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,9 @@
 import React from "react"
 import ReactDom from "react-dom/client"
 import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Body from "./components/Body"
-
-var datamart = [
-    {
-        title : "HTML",
-        des : "hyper text markup language"
-    },
-    {
-        title : "CSS",
-        des : "casecading style sheet"
-    },
-    {
-        title : "JS",
-        des : "Javascript"
-    },
-    {
-        title : "React.js",
-        des : "javascript UI library"
-    }
-]
 
 
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/>
-
-{
-    datamart.map((item,index)=>{
-        return(
-            <Body title={item.title} des={item.des} key={item.index}/>
-        )
-    })
-}
-
-<Footer/></>)
+reactroot.render(<><Header/></>)
 
 
