90518401f6d09fffa53322cd4f62a65e55974219
sasivarman-s <sureshraman2680@gmail.com>
2026-08-25 10:00:34 +0530

    simple number incrementing through useffect() when clicking name change button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,29 @@
 import React, { useState } from "react"
 import ReactDom from "react-dom/client"
 import "./main.css"
-import { stringify } from "postcss"
+import { useEffect } from "react"
+
 
 
 
 function Header() {
-    var [userdata, setUserdata] = useState([])
-
-    var [user, setUser] = useState({
-        name : "",
-        age : ""
-    })
+    var [count, setCount] = useState(0)
+    var [name,setName] = useState("sasi")
+    useEffect(()=>{
+        setCount(count+1)
+    },[name])
     return (
         <>
-            <div className="p-10 bg-lime-200 space-y-5 rounded-lg max-w-[70%] mx-auto">
-                <div>
-                    <label> Name:</label>
-                    <input placeholder="enter your name?" className="p-2 outline-none rounded-md" onChange={(e)=>{
-                        
-                        setUser((prev)=>{
-                            return {...prev, name : e.target.value}
-                        })
-                    }}></input>
-                </div>
-                <div>
-                    <label> age:</label>
-                    <input placeholder="enter your age?" className="p-2 outline-none rounded-md" onChange={(e)=>{
-                        setUser((prev)=>{
-                            return {...prev,age: e.target.value}
-                        })
-                    }} ></input>
-                </div>
-                <div>
-                    <button className="bg-purple-600 text-2xl text-white p-2 rounded" onClick={()=>{
-                        setUserdata((prev)=>{
-                            return [...prev,user]
-                        })
-                    }} >post!</button>
-                </div>
-                {
-                    userdata.map((item,i)=>{
-                        return (
-                            <>
-                            <div className="p-2 m-1 bg-cyan-300 rounded-lg w-[200px] h-[200px]">
-                                <h1 className="text-white text-center text-3xl">{item.name}</h1>
-                                <h2 className="text-white text-center text-2xl">{item.age}</h2>
-                            </div>
-                            </>
-                        )
-                    })
-                }
+        <div className="p-5 bg-lime-400 m-[50px] rounded-lg w-[200px] space-y-3  ">
+            <h1 className="text-3xl text-white">Count</h1>
+            <h1 className="bg-black text-white p-2 rounded-full text-center text-2xl">{count}</h1>
+            <div className="p-10 border-r-yellow-900">
+                <h1 className="text-white p-1 text-2xl">{name}</h1>
+                <button className="bg-emerald-300 p-2 rounded-lg text-white" onClick={()=>{
+                    setName("sasivarman")
+                }}>change.</button>
             </div>
+        </div>
         </>
     )
 }
