c454c0fad3c92aaa6f8bb5151af164893afc39be
sasivarman-s <sureshraman2680@gmail.com>
2026-08-25 14:06:27 +0530

    till process written

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import ReactDom from "react-dom/client"
-import Header from "./components/Header"
+import Header  from "./components/Header"
 import Footer from "./components/Footer"
 
 
+
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/><Footer/></>)
+reactroot.render(<><Header/></>)
 
 
