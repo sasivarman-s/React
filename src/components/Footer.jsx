a896be4e38f571de72cf4a5af6bf2405b0b4d378
sasivarman-s <sureshraman2680@gmail.com>
2026-08-25 10:04:36 +0530

    simple number incrementing through useffect() when clicking name change button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer(){
         <div className="max-w-[100%] bg-amber-200">
             <div className="w-[80%] mx-auto flex flex-col items-center justify-center">
                 <h1 className="text-blue-500 text-4xl text-center">To more follow this link!</h1>
-                <a href="https://sasivarman-portfolio.vercel.app/" className="text-blue-500 text-center">click to go to developer portfolio</a>
+                <a href="https://sasivarman-portfolio.vercel.app/" className="text-white text-center bg-fuchsia-600  rounded-lg p-1">click to go to developer portfolio</a>
             </div>
         </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import ReactDom from "react-dom/client"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/></>)
+reactroot.render(<><Header/><Footer/></>)
 
 
