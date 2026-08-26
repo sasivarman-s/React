8b24ee4ec229960f439a1e4bdad5708a05718391
sasivarman-s <sureshraman2680@gmail.com>
2026-08-26 21:40:27 +0530

    React final practice code

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,41 @@
 import React from "react"
 import ReactDom from "react-dom/client"
-import Header  from "./components/Header"
+import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Body from "./components/Body"
+import Homepage from "./components/Homepage"
+import Error from "./components/Error"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
 
+const Applayout = () => {
+    return (
+        <>
+            <Header />
+            <Outlet/>
+            <Footer />
+        </>
+    )
+}
 
+const Route = createBrowserRouter([
+    {
+        path: "/",
+        element: <Applayout />,
+        children: [
+            {
+                path: "/plan",
+                element: <Homepage />
+            },
+            {
+                path: "/home",
+                element: <Body />
+            }
+        ],
+        errorElement : <Error/>
+    }
+])
 
 const reactroot = ReactDom.createRoot(document.getElementById("root"))
-reactroot.render(<><Header/></>)
+reactroot.render(<><RouterProvider router={Route} /></>)
 
 
